9bba38a4ac0dece348871b1b9b847691fac218ef
Juthy <69724408+Juthy07@users.noreply.github.com>
2021-02-17 21:07:24 +0530

    Update filtered-ls.js

--- filtered-ls.js
+++ filtered-ls.js
@@ -1,6 +1,12 @@
+//Filtered LS
+//Program that prints a list of files in a given directory, filtered by the extension of the files. 
+//Provided a directory name as the first argument to the program  and a file extension to filter by as the second argument.
+//The list of files should be printed to the console, one file per line. Must use asynchronous I/O.
+
 "use strict"
 const fs = require('fs');
 const path = require('path');
+//Path module provides a way of working with directories and file paths. 
 
 const folder =  process.argv[2];
 const ext = '.' + process.argv[3];
