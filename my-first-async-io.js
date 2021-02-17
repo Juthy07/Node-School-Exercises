56af23ecfb25f0e5d8b9adf1dc3766a2391cda40
Juthy <69724408+Juthy07@users.noreply.github.com>
2021-02-17 20:45:42 +0530

    Update my-first-async-io.js
    Added explanations

--- my-first-async-io.js
+++ my-first-async-io.js
@@ -1,8 +1,13 @@
+//My First Async I/O
+//Program that uses a single asynchronous filesystem operation to read a file and print the number of newlines it contains to the console.
+
 "use strict"
 const fs = require('fs');
 const file = process.argv[2];
 
-fs.readFile(file, function (err, contents) { //contents - Buffer arguments
+fs.readFile(file, function (err, contents) { 
+//fs.readFile() method is used to read a file in a non-blocking asynchronous way.
+//contents - Buffer arguments
 
     if (err) {    //for any errors
        return console.log(err);
