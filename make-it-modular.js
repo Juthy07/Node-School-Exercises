0471cba209d3690cf6ac433e8df3086d8729c167
Juthy07 <juthy.shaji@gmail.com>
2021-02-18 19:32:07 +0530

    Updated a few lines.

--- make-it-modular.js
+++ make-it-modular.js
@@ -1,11 +1,19 @@
+ //Make It Modular
+ // Create a program that prints a list of files in a given directory, filtered by the extension of the files.
+ // The first argument is the directory name and the second argument is the extension filter.
+ // Print the list of files (one file per line) to the console. You must use asynchronous I/O.
+ 
  'use strict'
- let worker = require('./mymodule');
- let callback = function(err, list) {
+
+ let mymodule = require('./mymodule');
+ // must specify ./ as a path of root folder to import a local module.
+ let dir = process.argv[2];
+ let ext = process.argv[3];
+ mymodule(dir, ext, function(err, list) {
      if(err) {
          return console.error(err);
      }
      list.forEach(function(file) {
          console.log(file);
      });
- }
- worker(process.argv[2], process.argv[3], callback);
+ });
--- mymodule.js
+++ mymodule.js
@@ -1,6 +1,8 @@
 'use strict'
+
 const fs = require('fs');
 const path = require('path');
+
 module.exports = function (dir, ext, callback) {
     fs.readdir(dir, function (err, list) {
         if(err) {
@@ -11,5 +13,12 @@ module.exports = function (dir, ext, callback) {
         })
     
     callback(null, list)
+    //A callback is a function called at the completion of a given task; 
+    //this prevents any blocking, and allows other code to be run in the meantime.
     });
 };
+
+//The module is a variable that represents the current module, 
+//and exports is an object that will be exposed as a module. 
+//So, whatever you assign to module.exports will be exposed as a module.
+//REFERENCE : https://www.tutorialsteacher.com/nodejs/nodejs-module-exports
